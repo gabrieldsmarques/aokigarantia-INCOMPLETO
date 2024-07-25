fa1321a66a1a13c256e352e56126b41035d0ef25
gabrieldsmarques <you@example.com>
2024-07-25 16:12:17 -0400

    codigo pra comparar o texto do print com a tabela

--- adicionador.py
+++ adicionador.py
@@ -12,14 +12,16 @@ import pandas
 import plotly
 import pytesseract ##pytesseract serve pra reconhecer texto em imagens
 import cv2 ##o opencv serve pra ler e processar imagens
-#pytesseract.pytesseract.tesseract_cmd = 'c:\Users\POSITIVO\AppData\Local\Programs\Python\Python312\Scripts\pytesseract.exe'  ##definindo o caminho até o pytesseract
+import thefuzz as fuzz ##serve pra comparar textos
+
+pytesseract.pytesseract.tesseract_cmd = 'c:\Users\POSITIVO\AppData\Local\Programs\Python\Python312\Scripts\pytesseract.exe'  ##definindo o caminho até o pytesseract
 
 
 pyautogui.PAUSE = 0.2 ## deixa um intervalo de tempo entre todos os códigos de pyautogui, pro código n fazer tudo rápido demais e o pc não acompanhar
 pyautogui.sleep = 5
 
 tabela = pandas.read_csv('sucateamento.csv')
-tabela.drop_duplicates(subset=['sg'])
+
 
 for linha in tabela.index: 
     count = len(str(tabela.loc[linha, "sg"]))
@@ -29,10 +31,12 @@ for linha in tabela.index:
         sg = sg[:4] ##transformando a linha numa variável pra facilitar a vida
     pyautogui.write(sg)
     pyautogui.press('enter')
-   
-
-    ##image = pyautogui.screenshot() ##tira print da tela pra guardar na memória
-    ##img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) ##converte a imagem pra cinza pra acelerar o processo de identificação do texto
-    ##ret, thresh = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO) ##mais processamento de imagem pra acelerar o processo
-    ##codigo= pytesseract.image_tostring(img)
+    image = pyautogui.screenshot() ##tira print da tela pra guardar na memória
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) ##converte a imagem pra cinza pra acelerar o processo de identificação do texto
+    ret, thresh = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO) ##mais processamento de imagem pra acelerar o processo
+    codigo= pytesseract.image_tostring(img)
+    if fuzz.ratio(num, codigo) > 90: ##comparar o codigo da peça do print com o da tabela
+        pyautogui.locateOnScreen()
+    
+    
     
